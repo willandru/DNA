167fb59c583d87f8d0e3e7cc79a1f4effb5278d4
willandru <willandru@hotmail.com>
2023-12-23 13:28:39 -0500

    FASTA - counting number of aminoacids

--- Mycoplasmoides genitalium/fasta.py
+++ Mycoplasmoides genitalium/fasta.py
@@ -11,24 +11,29 @@ with open(path_fna, 'r') as file:
     file.seek(0)
     i=0
     a=0
+    AA=0
     old_len_aa= None
+
     for line in file:
         aa_per_line=len(line.strip())
         first_character=line.strip()[0]
+
         if(i==0 and first_character == '>'):
             print("GOOD :: the first line of the file starts with '>' ")
             a+=1
+
+        if(i>0):
+            AA+=aa_per_line
                     
         
-
         if(i>1 ):
+            
             if(aa_per_line != old_len_aa and i < num_rows-2 ):
                 print("BAD :: The file HAS NOT the same number of Amino Acids in each line")
                 print(i)
                 break
             if(i== num_rows-2):
                 print("GOOD :: the file has the same number of AA in each line")
-                print(f"Linea actual: {i} (antepenultima linea)")
                 a+=1
         
         old_len_aa=aa_per_line
@@ -36,4 +41,5 @@ with open(path_fna, 'r') as file:
     
     if(a==2):
         print('GOOD :: Valid FASTA file¡¡')
+        print(f"Amino Acids = {AA}")
         
--- Mycoplasmoides genitalium/ncbi_Mycoplasmoides_genitalium/ncbi_dataset/data/GCA_000027325.1/fasta.py
+++ Mycoplasmoides genitalium/ncbi_Mycoplasmoides_genitalium/ncbi_dataset/data/GCA_000027325.1/fasta.py
@@ -11,17 +11,23 @@ with open(path_fna, 'r') as file:
     file.seek(0)
     i=0
     a=0
+    AA=0
     old_len_aa= None
+
     for line in file:
         aa_per_line=len(line.strip())
         first_character=line.strip()[0]
+
         if(i==0 and first_character == '>'):
             print("GOOD :: the first line of the file starts with '>' ")
             a+=1
+
+        if(i>0):
+            AA+=aa_per_line
                     
         
-
         if(i>1 ):
+            
             if(aa_per_line != old_len_aa and i < num_rows-2 ):
                 print("BAD :: The file HAS NOT the same number of Amino Acids in each line")
                 print(i)
@@ -36,4 +42,5 @@ with open(path_fna, 'r') as file:
     
     if(a==2):
         print('GOOD :: Valid FASTA file¡¡')
+        print(f"Amino Acids = {AA}")
         
