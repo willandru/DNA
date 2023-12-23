ac8926bfc0ffb7323168efab363b295602235cf5
willandru <willandru@hotmail.com>
2023-12-23 14:34:05 -0500

    FASTA - NitrogenBases

--- Mycoplasmoides genitalium/fasta.py
+++ Mycoplasmoides genitalium/fasta.py
@@ -11,11 +11,12 @@ with open(path_fna, 'r') as file:
     file.seek(0)
     i=0
     a=0
-    AA=0
-    old_len_aa= None
+    NB=0
+    old_len_bn= None
+    nitrogen_bases=[]
 
     for line in file:
-        aa_per_line=len(line.strip())
+        bn_per_line=len(line.strip())
         first_character=line.strip()[0]
 
         if(i==0 and first_character == '>'):
@@ -23,23 +24,47 @@ with open(path_fna, 'r') as file:
             a+=1
 
         if(i>0):
-            AA+=aa_per_line
+            NB+=bn_per_line
+            for nbase in line:
+                if(nbase != '\n'):
+                    nitrogen_bases.append(nbase)
                     
         
         if(i>1 ):
             
-            if(aa_per_line != old_len_aa and i < num_rows-2 ):
-                print("BAD :: The file HAS NOT the same number of Amino Acids in each line")
+            if(bn_per_line != old_len_bn and i < num_rows-2 ):
+                print("BAD :: The file HAS NOT the same number of Nitrogen BAses in each line")
                 print(i)
                 break
             if(i== num_rows-2):
-                print("GOOD :: the file has the same number of AA in each line")
+                print("GOOD :: the file has the same number of Nitrogen Bases in each line")
                 a+=1
         
-        old_len_aa=aa_per_line
+        old_len_bn=bn_per_line
         i+=1
     
     if(a==2):
         print('GOOD :: Valid FASTA file¡¡')
-        print(f"Amino Acids = {AA}")
-        
+        print(f"Nitrogen Bases = {NB}")
+
+#Vamos a iniciar por definir los 64 codones posibles, y vamos a crear un dccionario con estos codones para contar cuantas 
+#veces aparecen en el FASTA     
+
+codons = ['AAA', 'AAC', 'AAG', 'AAT', 'ACA', 'ACC', 'ACG', 'ACT', 'AGA', 'AGC', 'AGG', 'AGT', 'ATA', 'ATC', 'ATG', 'ATT',
+          'CAA', 'CAC', 'CAG', 'CAT', 'CCA', 'CCC', 'CCG', 'CCT', 'CGA', 'CGC', 'CGG', 'CGT', 'CTA', 'CTC', 'CTG', 'CTT',
+          'GAA', 'GAC', 'GAG', 'GAT', 'GCA', 'GCC', 'GCG', 'GCT', 'GGA', 'GGC', 'GGG', 'GGT', 'GTA', 'GTC', 'GTG', 'GTT',
+          'TAA', 'TAC', 'TAG', 'TAT', 'TCA', 'TCC', 'TCG', 'TCT', 'TGA', 'TGC', 'TGG', 'TGT', 'TTA', 'TTC', 'TTG', 'TTT']
+
+codon_dictionary= {codon: 0 for codon in codons}
+
+
+n = len(nitrogen_bases)
+
+for i in range(2,n):
+    codon= nitrogen_bases[i-2] + nitrogen_bases[i-1] + nitrogen_bases[i]
+    if (codon in codon_dictionary):
+        codon_dictionary[codon] += 1
+
+# Print the codon counts
+for codon, count in codon_dictionary.items():
+    print(f"{codon}: {count} occurrences")
--- Mycoplasmoides genitalium/ncbi_Mycoplasmoides_genitalium/ncbi_dataset/data/GCA_000027325.1/fasta.py
+++ Mycoplasmoides genitalium/ncbi_Mycoplasmoides_genitalium/ncbi_dataset/data/GCA_000027325.1/fasta.py
@@ -12,10 +12,10 @@ with open(path_fna, 'r') as file:
     i=0
     a=0
     AA=0
-    old_len_aa= None
+    old_len_bn= None
 
     for line in file:
-        aa_per_line=len(line.strip())
+        bn_per_line=len(line.strip())
         first_character=line.strip()[0]
 
         if(i==0 and first_character == '>'):
@@ -23,12 +23,12 @@ with open(path_fna, 'r') as file:
             a+=1
 
         if(i>0):
-            AA+=aa_per_line
+            AA+=bn_per_line
                     
         
         if(i>1 ):
             
-            if(aa_per_line != old_len_aa and i < num_rows-2 ):
+            if(bn_per_line != old_len_bn and i < num_rows-2 ):
                 print("BAD :: The file HAS NOT the same number of Amino Acids in each line")
                 print(i)
                 break
@@ -37,7 +37,7 @@ with open(path_fna, 'r') as file:
                 print(f"Linea actual: {i} (antepenultima linea)")
                 a+=1
         
-        old_len_aa=aa_per_line
+        old_len_bn=bn_per_line
         i+=1
     
     if(a==2):
